453b48286e3587c55af2ad1b53b4d0deff9215a0
Ahmadlhm <lhmahmad2@gmail.com>
2025-06-01 11:38:05 +0300

    Add files via upload

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,1 +1,30 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('themeToggleBtn');
+    const body = document.body;
 
+    function setTheme(theme) {
+        if (theme === 'dark') {
+            body.classList.add('dark');
+            body.classList.remove('light');
+        } else {
+            body.classList.add('light');
+            body.classList.remove('dark');
+        }
+        localStorage.setItem('theme', theme);
+    }
+
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme) {
+        setTheme(savedTheme);
+    } else {
+        setTheme('light'); 
+    }
+
+    btn.addEventListener('click', () => {
+        if (body.classList.contains('dark')) {
+            setTheme('light');
+        } else {
+            setTheme('dark');
+        }
+    });
+});
